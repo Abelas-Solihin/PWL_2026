dcbbd81138bb1d0aaa02d429d1385a1ccd60ab62
Abelas Solihin <abelassolihin@gmail.com>
2026-04-08 22:29:23 +0700

    baru

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/world', function () { 
+return 'World'; 
+}); 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view ('welcome');
 });
+
+Route::get ('/', function() {
+    return ('Selamat Datang');
+});
+
+Route::get('/NIM_Nama', function(){
+    return 'Nim: 244107020052 Nama: Abelas Solihin';
+});
+
+Route::get('/user/{name}', function ($name) { 
+return 'Nama Saya: '.$name; 
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) { 
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+}); 
+
+Route::get('/articles/{id}', function ($id,){
+    return "Halaman Artikel dengan ID" .$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) { 
+    return 'Nama saya '.$name; 
+});
+
+Route::get('/user/{name?}', function($name='John'){
+     return 'Nama Saya ' .$name;
+});
+
+
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
